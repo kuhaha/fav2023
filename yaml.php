5ecc64cbc5509b1c2f2356d343af39287198d65e
kuhaha <kevin.yr.cheng@gmail.com>
2023-06-05 10:41:45 +0900

    Update yaml.php

--- yaml.php
+++ yaml.php
@@ -4,7 +4,7 @@ use \Symfony\Component\Yaml\Yaml;
 
 header('Content-Type: text/plain; charset=UTF-8');
 
-echo "=======dat/calendar.yaml============"
+echo "=======dat/calendar.yaml============\n";
 $input = file_get_contents("dat/calendar.yaml");
 $result = Yaml::parse($input);
 
@@ -13,14 +13,14 @@ var_dump($result);
 // print_r($result['priority']);
 // print_r($result['definitions']);
 
-echo "=======dat/reservation.yaml============"
+echo "=======dat/reservation.yaml============\n";
 
 $input = file_get_contents("dat/reservation.yaml");
 $result = Yaml::parse($input);
 var_dump($result);
 
 
-echo "=======dat/facilities.yaml============"
+echo "=======dat/facilities.yaml============\n";
 $input = file_get_contents("dat/facilities.yaml");
 $result = Yaml::parse($input);
 var_dump($result);
