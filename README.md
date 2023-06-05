7370d0b6800934830a76ba9c4aec413b6a6b92d3
kuhaha <kevin.yr.cheng@gmail.com>
2023-06-05 10:36:29 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+1. Composerをインストール
+
+2. fav2023フォルダに入って、以下のコマンドで、パッケージインストール
+  $ compose install
+
+3. yaml.phpを実行
+  http://localhost/fav2023/yaml.php
---
+++ yaml.php
@@ -0,0 +1,26 @@
+<?php 
+require "vendor/autoload.php";
+use \Symfony\Component\Yaml\Yaml;
+
+header('Content-Type: text/plain; charset=UTF-8');
+
+echo "=======dat/calendar.yaml============"
+$input = file_get_contents("dat/calendar.yaml");
+$result = Yaml::parse($input);
+
+var_dump($result);
+// echo $result['year'];
+// print_r($result['priority']);
+// print_r($result['definitions']);
+
+echo "=======dat/reservation.yaml============"
+
+$input = file_get_contents("dat/reservation.yaml");
+$result = Yaml::parse($input);
+var_dump($result);
+
+
+echo "=======dat/facilities.yaml============"
+$input = file_get_contents("dat/facilities.yaml");
+$result = Yaml::parse($input);
+var_dump($result);
